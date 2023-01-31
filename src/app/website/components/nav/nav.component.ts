82107c20afa3b664359726d950cd12838e8f6d91
Carlos-nicolas <carlosalexis52@hotmail.com>
2023-01-31 15:46:17 -0600

    redireccionar con guardianes

--- src/app/website/components/nav/nav.component.ts
+++ src/app/website/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { StoreService } from '../../../services/store.service'
 import { AuthService } from '../../../services/auth.service';
@@ -21,7 +22,8 @@ export class NavComponent implements OnInit {
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
-    private categoriesService:CategoriesService
+    private categoriesService:CategoriesService,
+    private router :Router,
   ) { }
 
   ngOnInit(): void {
@@ -49,4 +51,9 @@ export class NavComponent implements OnInit {
     })
   }
 
+  logout(){
+    this.authService.logout();
+    this.profile = null;
+    this.router.navigate(['/home'])
+  }
 }
